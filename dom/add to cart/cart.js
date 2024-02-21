a869b57f3fcd2cd557f94676d5f3f9754389422b
Mubee-fabevy <mubeena@fabevy.com>
2024-02-21 11:00:46 +0530

    feat: add to cart completed

--- dom/add to cart/cart.js
+++ dom/add to cart/cart.js
@@ -53,23 +53,74 @@ const array = [
     cusine: "north indian",
     img: "https://b.zmtcdn.com/data/dish_photos/065/8c61fca7367178d20b0a56b0b60dd065.jpg",
   },
-  { 
+  {
     id: 10,
-    foodName: 'paneer butter masala',
-    cusine: 'north indian',
-    img: 'hhttps://b.zmtcdn.com/data/dish_photos/065/8c61fca7367178d20b0a56b0b60dd065.jpg',
+    foodName: "paneer butter masala",
+    cusine: "north indian",
+    img: "https://b.zmtcdn.com/data/dish_photos/065/8c61fca7367178d20b0a56b0b60dd065.jpg",
   },
 ];
 
 const cont = document.querySelector(".container");
 
-const cards = document.createElement('div');
-cards.classList.add('row');
-cont.appendChild(cards);
-array.forEach(function(e){
-    const div = document.createElement('div');
-    cards.classList.add('col');
-    cards.appendChild(div);
-})
+//cart items
+const cartDiv = document.createElement("div");
+const cartHeading = document.createElement("h2");
+cartHeading.innerText = "Cart Items";
+cartDiv.classList.add("cart_container");
+document.body.appendChild(cartDiv);
+cartDiv.appendChild(cartHeading);
+array.forEach(function (e) {
+  //Creating Elements
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const btn = document.createElement("button");
 
+  div.classList.add("col");
 
+  //Set Attributes
+  img.setAttribute("src", e.img);
+
+  //cart
+  btn.setAttribute("id", "addtocart");
+  btn.setAttribute("onclick", "addToCart(this)");
+  div.setAttribute("data-id", e.id); //custom attribute
+  p1.innerText = `Food Name : ${e.foodName}`;
+  p2.innerText = `Cusine  : ${e.cusine}`;
+  p3.innerText = `ID  : ${e.id}`;
+  //cart
+  btn.innerText = "Add to Cart";
+  //Adding node element
+  div.appendChild(img);
+  div.appendChild(p1);
+  div.appendChild(p2);
+  div.appendChild(p3);
+  div.appendChild(btn);
+  cont.appendChild(div);
+});
+
+function addToCart(event) {
+  let a = event.parentElement.getAttribute("data-id");
+  console.log(a);
+  data = array.filter((e) => e.id == parseInt(a));
+  console.log(data);
+  const cartChildDiv = document.createElement("div");
+
+  const img = document.createElement("img");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  cartChildDiv.classList.add("col");
+  img.setAttribute("src", data[0].img);
+  p1.innerText = `Food Name : ${data[0].foodName}`;
+  p2.innerText = `Cusine  : ${data[0].cusine}`;
+  p3.innerText = `ID  : ${data[0].id}`;
+  cartChildDiv.appendChild(img);
+  cartChildDiv.appendChild(p1);
+  cartChildDiv.appendChild(p2);
+  cartChildDiv.appendChild(p3);
+  cartDiv.appendChild(cartChildDiv);
+}
